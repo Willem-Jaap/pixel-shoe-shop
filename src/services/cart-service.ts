8b251b2f7c2e8ba268e477636c825444ea89fdeb
Willem-Jaap <willemjaapk@gmail.com>
2024-10-20 14:53:10 +0200

    feat: add cart service & integration tests

---
+++ src/services/cart-service.ts
@@ -0,0 +1,125 @@
+import { Product } from '~/products-api';
+import { getProductPrice } from '~utils/prices';
+
+interface CartLine {
+    productId: number;
+    product: Product;
+    quantity: number;
+    basePrice: number;
+    discount: number;
+    vat: number;
+    totalPrice: number;
+    totalVat: number;
+}
+
+interface Cart {
+    lines: CartLine[];
+    customer: {
+        name: string;
+        email: string;
+        address: string;
+        discountPercentage: number;
+    };
+}
+
+// Cart service which implements the logic for adding products to the cart, calculating the total price, and generating the order summary
+class CartService {
+    public cart: Cart = {
+        lines: [],
+        customer: {
+            name: '',
+            email: '',
+            address: '',
+            discountPercentage: 0,
+        },
+    };
+
+    /**
+     * Add a product to the cart
+     *
+     * If the product is already in the cart, increase the quantity
+     */
+    addProduct(product: Product, quantity: number) {
+        const line = this.cart.lines.find(line => line.productId === product.id);
+
+        if (line) {
+            line.quantity += quantity;
+            // =========================================== //
+            // TUTORIAL: Comment out the following code block
+            // Remove the following code block
+            // =========================================== //
+            const { basePrice, discount, vat, totalPrice, totalVat } = getProductPrice(
+                product,
+                line.quantity,
+            );
+            line.basePrice = basePrice;
+            line.discount = discount;
+            line.vat = vat;
+            line.totalPrice = totalPrice;
+            line.totalVat = totalVat;
+            return;
+        }
+
+        const { basePrice, discount, vat, totalPrice, totalVat } = getProductPrice(
+            product,
+            quantity,
+        );
+        this.cart.lines.push({
+            productId: product.id,
+            product,
+            quantity,
+            basePrice,
+            discount,
+            vat,
+            totalPrice,
+            totalVat,
+        });
+    }
+
+    /**
+     * Remove a product from the cart by productId
+     */
+    removeProduct(productId: number) {
+        this.cart.lines = this.cart.lines.filter(line => line.productId !== productId);
+    }
+
+    /**
+     * Get the total price of the cart including VAT
+     */
+    getTotalPrice() {
+        return this.cart.lines.reduce((total, line) => total + line.totalPrice, 0);
+    }
+
+    /**
+     * Get the total VAT of the cart
+     */
+    getTotalVat() {
+        return this.cart.lines.reduce((total, line) => total + line.totalVat, 0);
+    }
+
+    /**
+     * Generate an order summary with the total price, total VAT, discount, and grand total
+     */
+    generateOrderSummary() {
+        const totalPrice = this.getTotalPrice();
+        const totalVat = this.getTotalVat();
+        const discount = totalPrice * this.cart.customer.discountPercentage;
+        const grandTotal = totalPrice - discount + totalVat;
+
+        return {
+            totalPrice,
+            totalVat,
+            discount,
+            grandTotal,
+        };
+    }
+
+    /**
+     * Set customer information for the cart
+     */
+    setCustomerInfo(customer: Cart['customer']) {
+        this.cart.customer = customer;
+    }
+}
+
+export default CartService;
---
+++ src/tests/integration/cart.test.ts
@@ -0,0 +1,132 @@
+import { Product } from '~/products-api';
+import CartService from '~/services/cart-service';
+
+describe('CartService', () => {
+    let cartService: CartService;
+
+    beforeEach(() => {
+        cartService = new CartService();
+    });
+
+    it('should add a product to the cart', () => {
+        const product = {
+            id: 1,
+            name: 'Product 1',
+            slug: 'product-1',
+            imageSrc: 'product-1.webp',
+            description: 'Description 1',
+            price: 100,
+            discount: 10,
+        };
+        cartService.addProduct(product, 2);
+
+        expect(cartService.cart.lines).toHaveLength(1);
+        expect(cartService.cart.lines[0].productId).toBe(1);
+        expect(cartService.cart.lines[0].quantity).toBe(2);
+        expect(cartService.cart.lines[0].basePrice).toBe(100);
+        expect(cartService.cart.lines[0].discount).toBe(0);
+        expect(cartService.cart.lines[0].vat).toBe(21);
+        expect(cartService.cart.lines[0].totalPrice).toBe(221);
+        expect(cartService.cart.lines[0].totalVat).toBe(42);
+    });
+
+    it('should increase the quantity of a product if it is already in the cart', () => {
+        const product: Product = {
+            id: 1,
+            name: 'Product 1',
+            slug: 'product-1',
+            imageSrc: 'product-1.webp',
+            description: 'Description 1',
+            price: 100,
+        };
+        cartService.addProduct(product, 2);
+        cartService.addProduct(product, 3);
+
+        expect(cartService.cart.lines).toHaveLength(1);
+        expect(cartService.cart.lines[0].quantity).toBe(5);
+        expect(cartService.cart.lines[0].basePrice).toBe(100);
+        expect(cartService.cart.lines[0].discount).toBe(10);
+        expect(cartService.cart.lines[0].vat).toBe(21);
+        expect(cartService.cart.lines[0].totalPrice).toBe(511);
+        expect(cartService.cart.lines[0].totalVat).toBe(105);
+    });
+
+    it('should add a customer to the cart', () => {
+        const customer = {
+            name: 'John Doe',
+            email: 'john-doe@gmail.com',
+            discountPercentage: 10,
+            address: '123 Main St',
+        };
+        cartService.setCustomerInfo(customer);
+
+        expect(cartService.cart.customer.name).toBe('John Doe');
+        expect(cartService.cart.customer.discountPercentage).toBe(10);
+        expect(cartService.cart.customer.address).toBe('123 Main St');
+    });
+
+    it('should remove a product from the cart', () => {
+        cartService.addProduct(
+            {
+                id: 1,
+                name: 'Product 1',
+                slug: 'product-1',
+                imageSrc: 'product-1.webp',
+                description: 'Description 1',
+                price: 100,
+            },
+            2,
+        );
+        cartService.addProduct(
+            {
+                id: 2,
+                name: 'Product 2',
+                slug: 'product-2',
+                description: 'Description 2',
+                imageSrc: 'product-2.webp',
+                price: 200,
+            },
+            3,
+        );
+
+        cartService.removeProduct(1);
+
+        expect(cartService.cart.lines).toHaveLength(1);
+        expect(cartService.cart.lines[0].productId).toBe(2);
+        expect(cartService.generateOrderSummary().totalPrice).toBe(642);
+    });
+
+    it('should get the total price of the cart', () => {
+        cartService.addProduct(
+            {
+                id: 1,
+                name: 'Product 1',
+                slug: 'product-1',
+                imageSrc: 'product-1.webp',
+                description: 'Description 1',
+                price: 100,
+            },
+            2,
+        );
+        cartService.addProduct(
+            {
+                id: 2,
+                name: 'Product 2',
+                slug: 'product-2',
+                description: 'Description 2',
+                imageSrc: 'product-2.webp',
+                price: 200,
+            },
+            3,
+        );
+
+        expect(cartService.getTotalPrice()).toBe(863);
+    });
+
+    // it('should get the total VAT of the cart', () => {
+    //     cartService.addProduct({ id: 1, name: 'Product 1', price: 100 }, 2);
+    //     cartService.addProduct({ id: 2, name: 'Product 2', price: 200 }, 3);
+
+    //     expect(cartService.getTotalVat()).toBe(120);
+    // });
+});
